003bf33e49e44b24d9d5e2bc4fe9d317fd8a4ea3
Patrick Luan <patrickluan2908@gmail.com>
2022-12-06 23:21:40 -0300

    Subiu os arquivos

--- ranking/ranking.c
+++ ranking/ranking.c
@@ -6,6 +6,10 @@ void salvarJogadorRanking(int pontos, char nome[20]) {
     FILE* arquivo;
 
     arquivo = fopen("ranking/ranking.txt", "r+");
+    if(arquivo == 0) {
+        printf("Banco de dados de palavras não disponível\n\n");
+        exit(1);
+    }
 
     fseek(arquivo, 0, SEEK_END);
 
